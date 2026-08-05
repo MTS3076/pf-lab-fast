3a6d50e32bab85d54b916992864223da6f7b09fb
MUHAMMAD TAHA SHUAIB <imtaha2006@gmail.com>
2026-08-05 05:30:47 +0500

    Add files via upload

---
+++ Lab-05/LAB 05 Task.cpp
@@ -0,0 +1,127 @@
+//#include<stdio.h>
+//
+//int main(){
+//	char color;
+//    printf("Enter a color from(R,Y,G): ");
+//    scanf("%c",&color);
+//    if (color == 'R'){
+//    	printf("Stop");
+//    	}else{
+//	      if (color == 'Y'){
+//    		 printf("Caution");
+//    		}else{
+//			  if (color == 'G'){
+//    		    printf("Go");
+//				}else{
+//    		       	printf("Invalid color entered");
+//    			}
+//			}
+//		}
+//    return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main(){
+//	int age;
+//	printf("Enter your age : ");
+//	scanf("%d", &age);
+//	if (age < 13){
+//		printf("Child");
+//	     }else{
+//	  	    if (age < 20 ){
+//	  	   	 printf("Teenager");
+//	  	      }else{
+//	  	   		 if (age < 60 ){
+//	  	   			printf("Adult");
+//	  	   		      }else{
+//	  	   				 printf("Senior");
+//						 				 
+//				 }
+//			 }
+//	  }
+//	return 0;
+//}
+
+//#include<stdio.h>
+
+//int main(){
+//	int days;
+//	printf("Enter the number of days in the year : ");
+//	scanf("%d",&days);
+//	if (days % 2 != 0){
+//		printf("It is not a leap year");
+//	}else{
+//		printf("It is a leap year");
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main(){
+//	int withdraw_limit=500,withdraw_amount=0;
+//	printf("Enter the withdrawl amount: ");
+//	scanf("%d",&withdraw_amount);
+//	if (withdraw_amount <= withdraw_limit){
+//		if (withdraw_amount % 20 == 0){
+//			printf("Withdrawal approved");
+//	}else{
+//	     printf("Withdrawal denied");
+//	     
+//	}
+//		}
+//		return 0;
+//	}
+
+//#include<stdio.h>
+//
+//int main(){
+//	int number=0;
+//	printf("Enter a number : ");
+//	scanf("%d",&number);
+//	if (number % 5 ==0 && number % 3 ==0){
+//		printf("%d is divisible by both 5 and 3");
+//    }else
+//         printf("%d is not divisible by both 5 and 3",number);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main(){
+//	int num1=0,num2=0;
+//	printf("Enter Number 1 and Number 2 : ");
+//	scanf("%d %d",&num1,&num2);
+//	(num1 > num2) ? printf("Number 1 is maximum") : printf("Number 2 is maximum");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main(){
+//	int rating=0;
+//	printf("Enter the rating for the movie \"The Lion King\" : ");
+//	scanf("%d",&rating);
+//	switch (rating)
+//	{
+//		case 1 :
+//			printf("Terrible");
+//		break;
+//		case 2 :
+//			printf("Poor");
+//		break;
+//		case 3 :
+//			printf("Average");
+//		break;
+//		case 4 :
+//			printf("Good");
+//		break;
+//		case 5 :
+//			printf("Excellent");
+//		break;
+//		default :
+//		printf("Invalid Input");
+//	return 0 ;
+//    } 
+//}
